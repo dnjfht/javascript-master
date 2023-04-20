af6fb6150562ea9c58cdf8dff7ea1e46db4471bf
Yoo Seungmin <dnjfht@gmail.com>
2023-04-20 22:31:44 +0900

    구조 분해 할당

---
+++ .history/9.iterator/6.destructure_20230420222853.js
@@ -0,0 +1,5 @@
+// 구조 분해 할당 Destructuring Assignment
+// 데이터 뭉치(그룹화)를 쉽게 만들 수 있다
+const fruits = ["🍓", "🍑", "🍉", "🍒"];
+const [first, second, ...others] = fruits;
+console.log(fruits[0]);
---
+++ .history/9.iterator/6.destructure_20230420223112.js
@@ -0,0 +1,9 @@
+// 구조 분해 할당 Destructuring Assignment
+// 데이터 뭉치(그룹화)를 쉽게 만들 수 있다
+const fruits = ["🍓", "🍑", "🍉", "🍒"];
+const [first, second, ...others] = fruits;
+console.log(fruits[0]);
+// 🍓
+console.log(first); // 🍓
+console.log(second); // 🍑
+console.log(others); // [ '🍉', '🍒' ]
