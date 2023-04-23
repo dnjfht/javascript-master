deba24d63dfff98bc4e46bd5864d4789157f6563
Yoo Seungmin <dnjfht@gmail.com>
2023-04-23 20:10:54 +0900

    nullish

--- .history/11.operators/3.nullish_20230423200905.js
+++ .history/11.operators/3.nullish_20230423200905.js
@@ -5,10 +5,7 @@
 // || falshy한 경우 설정(할당) => null, undefined, 0, -0, ""
 
 let num = 0;
-console.log(num || "-1");
+console.log(num || -1);
 // num이 없을 때만 -1을 출력하고 싶음
 // num에는 현재 유효한 0이 있기 때문에 0을 출력해야 하는데
 // 0은 falshy 값이므로 ||를 사용할 경우 -1을 출력함.
-console.log(num ?? "-1");
-// nullish를 사용하여 num이 없을 때만 -1를 출력함.
-// 0은 유효값이기 때문에 0을 출력하게 됨.
