02ee972febae37492398f89aa54c186643d66953
Yoo Seungmin <dnjfht@gmail.com>
2023-04-24 14:24:35 +0900

    error-handling

---
+++ .history/12.주석,에러처리,모듈/2.error-handling_20230424142333.js
@@ -0,0 +1,14 @@
+// 에러를 어떻게 하면 좀 더 우아하게 처리할 수 있는지
+
+function readFile(path) {
+  return "파일의 내용";
+}
+
+function processFile(path) {
+  const content = readFile(path);
+
+  const result = "hi" + content;
+  return result;
+}
+
+processFile("경로");
