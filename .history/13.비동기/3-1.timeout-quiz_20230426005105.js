7ddb502d35ee2815bf32519df8dbebc87bd1fe18
Yoo Seungmin <dnjfht@gmail.com>
2023-04-26 01:03:01 +0900

    timeout-quiz

---
+++ .history/13.비동기/3-1.timeout-quiz_20230426005105.js
@@ -0,0 +1,2 @@
+// 주어진 secondes(초)가 지나면 callback 함수를 호출함
+// 단, seconds가 0보다 작으면 에러를 던지자!
---
+++ .history/13.비동기/3-1.timeout-quiz_20230426005912.js
@@ -0,0 +1,19 @@
+// 주어진 seconds(초)가 지나면 callback 함수를 호출함
+// 단, seconds가 0보다 작으면 에러를 던지자!
+function runInDelay(callback, seconds) {
+  if (!callback) {
+    throw new Error("callback 함수가 없습니다!");
+  }
+  if (!seconds && seconds < 0) {
+    throw new Error("초가 0초 미만입니다!");
+  }
+  setInterval(callback, seconds * 1000);
+}
+
+try {
+  ruinDelay(() => {
+    console.log("seconds 초가 지나면 callback 함수를 호출함!");
+  }, 2);
+} catch (err) {
+  console.log(err);
+}
