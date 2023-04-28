f5a137343c0c718bfafb0da5c553bb0bfabe6d6b
Yoo Seungmin <dnjfht@gmail.com>
2023-04-28 22:03:08 +0900

    async...await

--- .history/13.비동기/7.async_20230428220204.js
+++ .history/13.비동기/7.async_20230428220204.js
@@ -23,8 +23,6 @@ function getOrange() {
 // 바나나와 사과를 함께 가지고 오기
 
 // 함수 앞에 async를 붙이면 비동기처럼 작동을 하는데
-// 즉 이 함수를 호출하면 promise가 return이 되는데
-// 함수 내부에서는 조금 더 동기적인 코드를 작성할 수 있음.
 
 async function fetchFruits() {
   return getBanana() //
--- .history/13.비동기/7.async_20230428220229.js
+++ .history/13.비동기/7.async_20230428220229.js
@@ -24,7 +24,6 @@ function getOrange() {
 
 // 함수 앞에 async를 붙이면 비동기처럼 작동을 하는데
 // 즉 이 함수를 호출하면 promise가 return이 되는데
-// 함수 내부에서는 조금 더 동기적인 코드를 작성할 수 있음.
 
 async function fetchFruits() {
   return getBanana() //
